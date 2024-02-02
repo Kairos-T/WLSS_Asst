44f19a82fdc8949282b8b4c706ab970e0770003e
Kairos Tay <contact@kairostay.com>
2024-02-02 11:56:48 +0800

    feat: added usbguard setup script

---
+++ Linux/setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check for sudo permissions
+if [ "$EUID" -ne 0 ]
+  then echo "Please run this script as root!"
+  exit
+fi
+
+# Install USBGuard
+yum install usbguard -y
+
+# Create initial rule set
+usbguard generate-policy > /etc/usbguard/rules.conf
+
+# Enable and start USBGuard
+systemctl start usbguard.service
+
+# Enable USBGuard on boot
+systemctl enable usbguard.service
+
+# Check status of USBGuard
+systemctl is-active --quiet usbguard.service && echo "USBGuard is active." || echo "USBGuard is not active."
+
+# Create default ruleset to block all new USB devices
+usbguard generate-policy > /etc/usbguard/rules.conf
+echo block >> /etc/usbguard/rules.conf
+
+# Load the new ruleset
+#usbguard read-policy /etc/usbguard/rules.conf
+usbguard reload
+install -m 0600 -o root -g root /etc/usbguard/rules.conf 
+
+# Inform user of completion
+echo "USBGuard has been installed and configured."
